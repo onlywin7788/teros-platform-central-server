73b0fe6afb92d5fc34e3f97d0b462f8f5eb20a04
onlywin7788 <onlywin7788@gmail.com>
2020-12-03 15:07:24 +0900

    commit

--- src/main/java/com/teros/central_server/controller/advice/ExceptionAdvice.java
+++
@@ -1,9 +0,0 @@
-package com.teros.central_server.controller.advice;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-@RequiredArgsConstructor
-@RestControllerAdvice
-public class ExceptionAdvice {
-}
--- src/main/java/com/teros/central_server/repository/apim/product/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.teros.central_server.repository.apim.product;
-
-import com.teros.central_server.entity.apim.product.ProductEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
-}
