51f6f19a28e60a1931f48c402e543d1556958cd9
onlywin7788 <onlywin7788@gmail.com>
2020-12-04 13:09:09 +0900

    commit

--- src/main/java/com/teros/central_server/repository/apim/api/APIRepository.java
+++
@@ -1,7 +0,0 @@
-package com.teros.central_server.repository.apim.api;
-
-import com.teros.central_server.entity.apim.api.APIEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface APIRepository extends JpaRepository<APIEntity, Long> {
-}
