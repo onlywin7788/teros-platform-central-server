867b7741fc2c84c1d382b2115c10f2bc2d132e07
navana <onlywin7788@gmail.com>
2021-06-14 11:59:43 +0900

    commit

--- src/main/java/com/teros/central_server/config/swagger/SwaggerConfiguration.java
+++ src/main/java/com/teros/central_server/config/swagger/SwaggerConfiguration.java
@@ -10,7 +10,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-// connect => http://ip:port/swagger-ui.html
+// connect => http://ip:port/central-server/swagger-ui.html
 
 @Configuration
 @EnableSwagger2
